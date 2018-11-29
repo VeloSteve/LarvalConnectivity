3a410c4a6f785e509371863bb8b00318f73af0cd
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-11-29 11:21:15 -0800

    Add age classes and ecotypes
    So many coral populations can be tracked.

--- src/Coral.m
+++ src/Coral.m
@@ -6,14 +6,19 @@ classdef Coral < Population
     
     properties
         sym
+        ageClass
+        ecotype
     end
-    
+
     methods
         function obj = Coral(t0, startPopFraction, growthParams)
             %CORAL Construct a Coral
             %   This defines a particular coral type on a reef.
             obj@Population(t0, startPopFraction, growthParams.KC, growthParams);
             obj.sym = Symbiont(t0, 0.5, growthParams);
+            % Reefs are initialized with adult coral.
+            obj.ageClass = AgeClass.Adult;
+            obj.ecotype = growthParams.name;
         end
         
         function coral = copySubset(obj, t, fraction)
@@ -23,6 +28,7 @@ classdef Coral < Population
             % method directly, so just set everything.
             coral = Coral(t, fraction, obj.con);
             coral.setPop(t, fraction * obj.pop);
+            coral.ageClass = obj.ageClass;
             coral.sym = obj.sym.copySubset(t, fraction);
         end
             
@@ -31,6 +37,18 @@ classdef Coral < Population
             %SETGENOTYPE Set the optimal temperature and variance for this
             %Coral's Symbionts.
             obj.sym.setGenotype(t, gi, selV);
+        end
+        
+        function scalePopulation(obj, frac)
+        % SCALEPOPULATION Scale the population by frac, as when only part of a
+        % population recruits.  Assumes that symbionts scale by the same
+        % fraction.
+            obj.pop = obj.pop * frac;
+            obj.sym.pop = obj.sym.pop * frac;
+        end
+        
+        function nextAgeClass(obj)
+            obj.ageClass = AgeClass.next(obj.ageClass);
         end
         
         
