bf535ca809ecea439c44704077dfc54045827e96
Steve Ryan <1319496+VeloSteve@users.noreply.github.com>
2018-11-29 11:19:38 -0800

    Create a 2D array when there is >1 coral

--- src/@Reef/nonODEIterativeInputsSimplified.m
+++ src/@Reef/nonODEIterativeInputsSimplified.m
@@ -1,8 +1,10 @@
 % Calculate values used in the ODE iterations, but not dependent on them.
-function [ri] = nonODEIterativeInputsSimplified(sst, gi0, SelV, con)
+function [ri] = nonODEIterativeInputsSimplified(sst, gi0, SelV, con, nc)
     rmVec = con.a*exp(con.b*sst);
 
-    ri = (1- (con.EnvV + (min(0, gi0 - sst)).^2) ./ ...
-        (2*SelV)) .* exp(con.b*min(0, sst - gi0)) .* rmVec;
-    
+    ri(nc, length(sst)) = 0.0;
+    for i = 1:nc
+        ri(i, :) = (1- (con.EnvV + (min(0, gi0(i) - sst)).^2) ./ ...
+            (2*SelV(i))) .* exp(con.b*min(0, sst - gi0(i))) .* rmVec;
+    end
 end
